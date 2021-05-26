02d9c385ea874aeaafbdd92b0f6ed87b16b3e089
MunschSam <samuel.munsch@gmail.com>
2021-05-26 09:27:27 +0200

    quete 9

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,26 +5,34 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Program;
 
 class ProgramController extends AbstractController
 {
+
+
  /**
- * @Route("/programs/", name="program_index")
+  * @Route("/programs/{id}", requirements={"id"="\d+"}, name="program_show")
+  */
+public function show(int $id): Response
+{
+    return $this->render('program/show.html.twig', ['id' => $id]);
+}
+
+/**
+ * 
+ * @Route("/programs", name="program_index")
+ * @return Response A response instance
  */
 public function index(): Response
 {
-    return $this->render('program/index.html.twig', [
-       'website' => 'Wild Séries',
-    ]);
-}
+    $programs = $this->getDoctrine()
+             ->getRepository(Program::class)
+             ->findAll();
 
- /**
-  * @Route("/programs/4", requirements={"page"="\d+"}, name="program_show")
-  */
-public function show(int $page = 4): Response
-{
-    return $this->render('program/show.html.twig', ['page' => $page]);
-}
-
-
+         return $this->render(
+             'program/index.html.twig',
+             ['programs' => $programs]
+         );
+    }
 }
