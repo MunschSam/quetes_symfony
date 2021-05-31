7dffa116098dc7f5fe3f36abea453b90c1e2c9ac
MunschSam <samuel.munsch@gmail.com>
2021-05-31 14:58:18 +0200

    quete 10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
+use App\Entity\Season;
+use App\Entity\Category;
+use App\Entity\Episode;
 
 class ProgramController extends AbstractController
 {
@@ -14,7 +17,7 @@ class ProgramController extends AbstractController
 /**
  * Getting a program by id
  *
- * @Route("/show/{id<^[0-9]+$>}", name="show")
+ * @Route("program/show/{id<^[0-9]+$>}", name="program_show")
  * @return Response
  */
 public function show(int $id):Response
@@ -23,15 +26,44 @@ public function show(int $id):Response
         ->getRepository(Program::class)
         ->findOneBy(['id' => $id]);
 
+        $seasons = $program->getSeasons();
+
     if (!$program) {
         throw $this->createNotFoundException(
             'No program with id : '.$id.' found in program\'s table.'
         );
     }
     return $this->render('program/show.html.twig', [
-        'program' => $program,
+        'program' => $program, 'seasons' => $seasons
+
     ]);
     
+}
+/**
+ * Getting a episode by season and program
+ *
+ * @Route(" /program/show/{programId}/seasons/{seasonId}", name="season_show")
+ * @return Response
+ */
+public function showSeason(int $programId, int $seasonId)
+{
+    $program = $this->getDoctrine()
+         ->getRepository(Program::class)
+         ->findOneBy(['id' => $programId]);
+
+     $season = $this->getDoctrine()
+         ->getRepository(Season::class)
+         ->findOneBy(['id' => $seasonId]);
+    
+    /*
+
+    $episodes = $program->getSeasons()->getEpisodes();
+*/
+    return $this->render('program/season_show.html.twig', [
+        'program' => $program,
+        'season' => $season,
+        'episodes' => $season->getEpisodes(),
+    ]);
 }
 
 /**
