4a8d92d99f2c989f5e00835cba9d0710e6ef3859
MunschSam <samuel.munsch@gmail.com>
2021-06-02 10:25:27 +0200

    quete 14 validation

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,9 +6,17 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
+
+
+
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title", message="ce titre existe déjà")
  */
 class Program
 {
@@ -18,14 +26,17 @@ class Program
      * @ORM\Column(type="integer")
      */
     private $id;
-
+    
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="remplissez le titre ")
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="remplissez la description")
      */
     private $summary;
 
